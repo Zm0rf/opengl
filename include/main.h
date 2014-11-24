84969fe160e57bfbfce0001f226cd9cc0f1da3b7
Zmorf <ludde90@gmail.com>
2014-11-24 04:11:50 +0100

    Playing around with shaders

--- include/main.h
+++ include/main.h
@@ -1,2 +1,19 @@
+// Include standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
+// Include GLEW
+#include <GL/glew.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+#include <stdlib.h>
+#include <string.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
