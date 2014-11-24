f2b616f71ce4ec713d631d0a5dfaaeb30835a93d
Viktor Danhall <bricktror@gmail.com>
2014-11-24 19:14:27 +0100

    Refactoring test

--- test/dynamic_loading/main.cpp
+++ test/dynamic_loading/main.cpp
@@ -1,10 +1,11 @@
+/* #include "gtest/gtest.h" */
+
 #include <stdio.h>
 #include "dynamic_loading/BaseClass.h"
 #include "LinkedObjectFactory.h"
 
 int main(int argc, char** argv)
 {
-    printf("TEST compiled at %s %s\n", __DATE__, __TIME__);
     //
     LinkedObjectFactory<BaseClass> f(TEST_LIBRARY_PATH);
     if( !f.isLoaded() )
